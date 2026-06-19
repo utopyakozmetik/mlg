2235f61c506ca7bf9b4d77f33060b7a249565776
utopyakozmetik <ltgkptn@gmail.com>
2026-06-19 15:51:57 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,91 @@
-player.src = videos[index].iframe;
+let videos = [];
+let current = 0;
+
+const player =
+document.getElementById("player");
+
+const channelText =
+document.getElementById("channelText");
+
+async function init() {
+
+    try {
+
+        const response =
+        await fetch("videos.json");
+
+        videos =
+        await response.json();
+
+        if(!videos.length){
+            return;
+        }
+
+        loadVideo(0);
+
+    } catch(err){
+
+        console.error(err);
+
+    }
+
+}
+
+function loadVideo(index){
+
+    if(index < 0){
+        index = videos.length - 1;
+    }
+
+    if(index >= videos.length){
+        index = 0;
+    }
+
+    current = index;
+
+    player.src =
+    videos[index].iframe;
+
+    channelText.innerHTML =
+    `VIDEO ${index + 1} / ${videos.length}`;
+
+}
+
+function nextVideo(){
+    loadVideo(current + 1);
+}
+
+function prevVideo(){
+    loadVideo(current - 1);
+}
+
+document
+.getElementById("nextBtn")
+.addEventListener(
+"click",
+nextVideo
+);
+
+document
+.getElementById("prevBtn")
+.addEventListener(
+"click",
+prevVideo
+);
+
+document.addEventListener(
+"keydown",
+(e)=>{
+
+    if(e.key==="ArrowRight"){
+        nextVideo();
+    }
+
+    if(e.key==="ArrowLeft"){
+        prevVideo();
+    }
+
+}
+);
+
+init();
