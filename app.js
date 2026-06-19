db19c978b33927dd7daa50ec09cef5a0be668599
utopyakozmetik <ltgkptn@gmail.com>
2026-06-19 17:23:08 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,54 +1,28 @@
 let videos = [];
 let current = 0;
 
-const player =
-document.getElementById("player");
-
-const channelText =
-document.getElementById("channelText");
+const player = document.getElementById("player");
+const channelText = document.getElementById("channelText");
 
 async function init() {
+    const res = await fetch("videos.json");
+    videos = await res.json();
 
-    try {
+    if(!videos.length) return;
 
-        const response =
-        await fetch("videos.json");
-
-        videos =
-        await response.json();
-
-        if(!videos.length){
-            return;
-        }
-
-        loadVideo(0);
-
-    } catch(err){
-
-        console.error(err);
-
-    }
-
+    loadVideo(0);
 }
 
 function loadVideo(index){
 
-    if(index < 0){
-        index = videos.length - 1;
-    }
-
-    if(index >= videos.length){
-        index = 0;
-    }
+    if(index < 0) index = videos.length - 1;
+    if(index >= videos.length) index = 0;
 
     current = index;
 
-    player.src =
-    videos[index].iframe;
+    player.src = videos[index].iframe;
 
-    channelText.innerHTML =
-    `VIDEO ${index + 1} / ${videos.length}`;
-
+    channelText.innerHTML = `VIDEO ${index + 1} / ${videos.length}`;
 }
 
 function nextVideo(){
@@ -59,33 +33,12 @@ function prevVideo(){
     loadVideo(current - 1);
 }
 
-document
-.getElementById("nextBtn")
-.addEventListener(
-"click",
-nextVideo
-);
+document.getElementById("nextBtn").addEventListener("click", nextVideo);
+document.getElementById("prevBtn").addEventListener("click", prevVideo);
 
-document
-.getElementById("prevBtn")
-.addEventListener(
-"click",
-prevVideo
-);
-
-document.addEventListener(
-"keydown",
-(e)=>{
-
-    if(e.key==="ArrowRight"){
-        nextVideo();
-    }
-
-    if(e.key==="ArrowLeft"){
-        prevVideo();
-    }
-
-}
-);
+document.addEventListener("keydown", (e)=>{
+    if(e.key === "ArrowRight") nextVideo();
+    if(e.key === "ArrowLeft") prevVideo();
+});
 
 init();
